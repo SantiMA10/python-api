2359366a03506bfc920beab38bf72e6b34c13948
Santiago Martín Agra <santiagomartinagra@gmail.com>
2025-11-15 18:08:27 +0100

    feat: add create user use case

--- tests/unit/delivery/api/v1/test_users_router.py
+++ tests/unit/delivery/api/v1/test_users_router.py
@@ -1,16 +1,32 @@
 from http.client import CREATED
 
 from expects import expect, equal
+from doublex import Mimic, Spy
+from doublex_expects import have_been_called_with
 from fastapi.testclient import TestClient
 
 from main import app
+from src.use_cases.commands.create_user_command import (
+    CreateUserCommand,
+    CreateUserCommandHandler,
+)
+from src.domain.user import User
+from src.delivery.api.v1.users.users_router import _get_create_user_command_handler
 
 
 class TestUsersRouter:
     def test_create_user(self) -> None:
-        payload = {"name": "Jack", "age": 30}
+        name = "Jack"
+        age = 30
+        payload = {"name": name, "age": age}
+        user = User(name=name, age=age)
+        command = CreateUserCommand(user)
         client = TestClient(app)
+        handler = Mimic(Spy, CreateUserCommandHandler)
+
+        app.dependency_overrides[_get_create_user_command_handler] = lambda: handler
 
         response = client.post("/api/v1/users", json=payload)
 
         expect(response.status_code).to(equal(CREATED))
+        expect(handler.execute).to(have_been_called_with(command))
